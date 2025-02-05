1e31a47e6ade16b6764d083ef32bcb129bebf066
theo-ooooo <kkw.theo@gmail.com>
2025-02-05 14:49:46 +0900

    init, domain

---
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop;
+
+import org.h2.engine.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager(); // 요청시 매번 받아야한다.
+
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        try {
+
+            tx.commit();
+        }catch(Exception e) {
+            System.out.println("e" + e.getMessage());
+            tx.rollback();
+        } finally {
+            em.close();
+            emf.close();
+        }
+    }
+}
